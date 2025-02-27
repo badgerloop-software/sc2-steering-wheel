c82960f75c2f2a739985483993ca798dfde95871
Aditya YV <adityayv802@gmail.com>
2025-02-26 18:33:18 -0600

    Initial draft for IOManagement

--- include/IOManagement.h
+++ include/IOManagement.h
@@ -1,6 +1,17 @@
 #ifndef __IO_MANAGEMENT_H__
 #define __IO_MANAGEMENT_H__
 
+#include <Arduino.h>
+
+//Macros for pins
+#define DIRECTION_SWITCH_PIN        PA_9
+#define LEFT_BLINK_PIN              PB_1
+#define RIGHT_BLINK_PIN             PF_0
+#define CRZ_MODE_A_PIN              PF_1
+#define CRZ_SET_PIN                 PA_8
+#define CRZ_RESET_PIN               PB_5
+#define HORN_PIN                    PA_7
+#define REGEN_BRAKE_PIN             PA_4
 
 struct Digital_Data {
     bool direction_switch : 1;      // input
--- src/IOManagement.cpp
+++ src/IOManagement.cpp
@@ -1,9 +1,33 @@
 #include "IOManagement.h"
+#include "adc.h"
+
+volatile Digital_Data digital_data = {0, 0, 0, 0, 0, 0, 0};
+volatile float regen_brake = 0.0f;
 
 void initIO() {
+    // Initialize digital pins
+    pinMode(DIRECTION_SWITCH_PIN, INPUT);
+    pinMode(LEFT_BLINK_PIN, INPUT);
+    pinMode(RIGHT_BLINK_PIN, INPUT);
+    pinMode(CRZ_MODE_A_PIN, INPUT);
+    pinMode(CRZ_SET_PIN, INPUT);
+    pinMode(CRZ_RESET_PIN, INPUT);
+    pinMode(HORN_PIN, INPUT);
 
+    // Initialize analog pins
+    initADC(ADC1);
 }
 
 void readIO() {
-    
+    // Read digital inputs
+    digital_data.direction_switch = digitalRead(DIRECTION_SWITCH_PIN);
+    digital_data.left_blink = digitalRead(LEFT_BLINK_PIN);
+    digital_data.right_blink = digitalRead(RIGHT_BLINK_PIN);
+    digital_data.crz_mode_a = digitalRead(CRZ_MODE_A_PIN);
+    digital_data.crz_set = digitalRead(CRZ_SET_PIN);
+    digital_data.crz_reset = digitalRead(CRZ_RESET_PIN);
+    digital_data.horn = digitalRead(HORN_PIN);
+
+    // Read analog input
+    regen_brake = readADC(REGEN_BRAKE_PIN);
 }
