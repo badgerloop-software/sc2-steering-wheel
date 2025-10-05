da67c32fdb81068a854c5adcc3f9cfeffe14be2c
Will Wentink <wwentink@wisc.edu>
2025-10-05 14:11:27 -0500

    Initial working pointer commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include "display.h"
+#include "pointer.h"
 
 void setup() {
   Serial.begin(115200);
@@ -17,4 +18,20 @@ void loop() {
   // drawSdJpeg("/test.jpg", 0, 0);
 
   // HeapAnim();
+
+  // pointer loop
+  // if (Serial.available()) {
+  //       String input = Serial.readStringUntil('\n');
+  //       input.trim();
+
+  //       if (input.startsWith("a")) {                          // ex: use a10 to set to 10 degrees
+  //           double angle = input.substring(1).toDouble();
+  //           updatePointerAngle(angle);
+  //           Serial.printf("set to angle: %.2f deg\n", angle);
+  //       } else {
+  //           int value = input.toInt();                        // otherwise use integer method
+  //           updatePointer(value);
+  //           Serial.printf("set to value: %d\n", value);
+  //       }
+  //   }
 }
