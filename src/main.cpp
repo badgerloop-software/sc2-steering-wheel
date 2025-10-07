cced59b334e8da4b5793ac8e05733d67b1617012
bquan0 <powerbowen@gmail.com>
2025-10-06 19:50:39 -0500

    main.cpp: add debug printouts for digital_data.

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,20 @@ extern bool send_success;
 
 void setup() {
     Serial.begin(115200);
-    // initIO();
+    initIO();
 }
 
 void loop() {
     canSteering.sendSteeringData();
     canSteering.runQueue(1000);
     printf("send_success: %d\n", send_success);
+    printf("headlight: %d\n", digital_data.headlight);
+    printf("left blink: %d\n", digital_data.left_blink);
+    printf("right_blink: %d\n", digital_data.right_blink);
+    printf("direction_switch: %d\n", digital_data.direction_switch);
+    printf("horn: %d\n", digital_data.horn);
+    printf("crzmodea: %d\n", digital_data.crz_mode_a);
+    printf("crz_set: %d\n", digital_data.crz_set);
+    printf("crz_reset: %d\n", digital_data.crz_reset);
+    printf("number reads: %d\n", number_reads);
 }
