4377359c8378154b8a6295ef1094dea3b8f517b3
Ryan Sinnreich <sinnreich@wisc.edu>
2026-05-10 23:18:15 -0500

    Change odo to show tenths

--- src/display.cpp
+++ src/display.cpp
@@ -1,14 +1,15 @@
-#include <Arduino.h>
 #include "display.h"
 #include "IOManagement.h"
 #include "odometer.h"
+#include <Arduino.h>
 
 #include <math.h>
 
 TFT_eSPI tft = TFT_eSPI();
 
-//Array of random colors to cycle through
-uint16_t colors[] = {TFT_RED, TFT_GREEN, TFT_BLUE, TFT_YELLOW, TFT_CYAN, TFT_MAGENTA};
+// Array of random colors to cycle through
+uint16_t colors[] = {TFT_RED,    TFT_GREEN, TFT_BLUE,
+                     TFT_YELLOW, TFT_CYAN,  TFT_MAGENTA};
 
 void rotateColors() {
   static uint8_t colorIndex = 0;
@@ -16,78 +17,80 @@ void rotateColors() {
   colorIndex = (colorIndex + 1) % (sizeof(colors) / sizeof(colors[0]));
 }
 
-void initDisplay(bool SD_enable){
-    // Set all chip selects high to avoid bus contention during initialisation of each peripheral
-    // digitalWrite(22, HIGH); // Touch controller chip select (if used)
-    digitalWrite(15, HIGH); // TFT screen chip select
-    // digitalWrite( 5, HIGH); // SD card chips select, must use GPIO 5 (ESP32 SS)
+void initDisplay(bool SD_enable) {
+  // Set all chip selects high to avoid bus contention during initialisation of
+  // each peripheral digitalWrite(22, HIGH); // Touch controller chip select (if
+  // used)
+  digitalWrite(15, HIGH); // TFT screen chip select
+  // digitalWrite( 5, HIGH); // SD card chips select, must use GPIO 5 (ESP32 SS)
 
-    tft.begin();
-    tft.setRotation(1);  // Somehow important to have an odd rotation number
-                          //Display does not work properly if you do not set this and initialise the SD card
-    tft.fillScreen(TFT_BLACK);
+  tft.begin();
+  tft.setRotation(1); // Somehow important to have an odd rotation number
+                      // Display does not work properly if you do not set this
+                      // and initialise the SD card
+  tft.fillScreen(TFT_BLACK);
 
-    if (!SD_enable) {
+  if (!SD_enable) {
 #ifdef DEBUG_PRINTS
-      Serial.println("Display initialized without SD card.");
+    Serial.println("Display initialized without SD card.");
 #endif
-      return;
-    }
+    return;
+  }
 
-    if (!SD.begin(5, tft.getSPIinstance())) {
+  if (!SD.begin(5, tft.getSPIinstance())) {
 #ifdef DEBUG_PRINTS
-      Serial.println("Card Mount Failed");
+    Serial.println("Card Mount Failed");
 #endif
-      return;
-    }
-    uint8_t cardType = SD.cardType();
-  
-    if (cardType == CARD_NONE) {
-#ifdef DEBUG_PRINTS
-      Serial.println("No SD card attached");
-#endif
-      return;
-    }
-  
-#ifdef DEBUG_PRINTS
-    Serial.print("SD Card Type: ");
-    if (cardType == CARD_MMC) {
-      Serial.println("MMC");
-    } else if (cardType == CARD_SD) {
-      Serial.println("SDSC");
-    } else if (cardType == CARD_SDHC) {
-      Serial.println("SDHC");
-    } else {
-      Serial.println("UNKNOWN");
-    }
-#endif
+    return;
+  }
+  uint8_t cardType = SD.cardType();
 
-    // // Check if frame files need to be created from text
-    // bool needsConversion = false;
-    // for (int i = 0; i < 14; i++) {
-    //   sprintf(currentFramePath, FRAME_FILE_PATTERN, i);
-    //   if (!SD.exists(currentFramePath)) {
-    //     needsConversion = true;
-    //     break;
-    //   }
-    // }
-
-    // // Convert the text animation to individual binary frame files if needed
-    // if (needsConversion && SD.exists("/bsr/som.txt")) {
-    //   Serial.println("Creating individual binary frame files...");
-    //   convertTextToBinaryFrames("/bsr/som.txt");
-    // }
-  
-    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+  if (cardType == CARD_NONE) {
 #ifdef DEBUG_PRINTS
-    Serial.printf("SD Card Size: %lluMB\n", cardSize);
+    Serial.println("No SD card attached");
 #endif
-  
-    // countAvailableFrames();
-    // Serial.printf("Total frames available: %d\n", totalFrames);
+    return;
+  }
 
 #ifdef DEBUG_PRINTS
-    Serial.println("Initialization done.");
+  Serial.print("SD Card Type: ");
+  if (cardType == CARD_MMC) {
+    Serial.println("MMC");
+  } else if (cardType == CARD_SD) {
+    Serial.println("SDSC");
+  } else if (cardType == CARD_SDHC) {
+    Serial.println("SDHC");
+  } else {
+    Serial.println("UNKNOWN");
+  }
+#endif
+
+  // // Check if frame files need to be created from text
+  // bool needsConversion = false;
+  // for (int i = 0; i < 14; i++) {
+  //   sprintf(currentFramePath, FRAME_FILE_PATTERN, i);
+  //   if (!SD.exists(currentFramePath)) {
+  //     needsConversion = true;
+  //     break;
+  //   }
+  // }
+
+  // // Convert the text animation to individual binary frame files if needed
+  // if (needsConversion && SD.exists("/bsr/som.txt")) {
+  //   Serial.println("Creating individual binary frame files...");
+  //   convertTextToBinaryFrames("/bsr/som.txt");
+  // }
+
+  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+#ifdef DEBUG_PRINTS
+  Serial.printf("SD Card Size: %lluMB\n", cardSize);
+#endif
+
+  // countAvailableFrames();
+  // Serial.printf("Total frames available: %d\n", totalFrames);
+
+#ifdef DEBUG_PRINTS
+  Serial.println("Initialization done.");
 #endif
 }
 
@@ -101,7 +104,8 @@ static void drawBoolLabel(int x, int y, const char *label, bool value) {
   tft.print(value ? "ON" : "OFF");
 }
 
-static void drawStatusPill(int x, int y, int w, int h, const char *label, bool value, uint16_t color) {
+static void drawStatusPill(int x, int y, int w, int h, const char *label,
+                           bool value, uint16_t color) {
   tft.drawRoundRect(x, y, w, h, 6, color);
   tft.fillRect(x + 1, y + 1, w - 2, h - 2, value ? color : TFT_BLACK);
   tft.setTextSize(2);
@@ -152,7 +156,8 @@ void renderMinimalDisplay(float speed) {
     accelVoltage = 2.814f;
   }
 
-  int accelPercent = (int)lroundf(((accelVoltage - 0.618f) / (2.814f - 0.618f)) * 100.0f);
+  int accelPercent =
+      (int)lroundf(((accelVoltage - 0.618f) / (2.814f - 0.618f)) * 100.0f);
   if (accelPercent < 0) {
     accelPercent = 0;
   } else if (accelPercent > 100) {
@@ -175,22 +180,16 @@ void renderMinimalDisplay(float speed) {
   bool cruiseReset = digital_data.crz_reset;
   uint8_t currentDriveMode = drive_mode;
   bool directionSwitch = digital_data.direction_switch;
-  uint32_t currentOdo = getOdometerMiles();
+  uint32_t currentOdo = getOdometerTenths();
 
-  bool screenNeedsUpdate = !hasPreviousFrame ||
-                           speedValue != lastSpeedValue ||
-                           accelPercent != lastAccelPercent ||
-                           regenPercent != lastRegenPercent ||
-                           headlight != lastHeadlight ||
-                           leftBlink != lastLeftBlink ||
-                           rightBlink != lastRightBlink ||
-                           hazardState != lastHazards ||
-                           cruiseMode != lastCruiseMode ||
-                           cruiseSet != lastCruiseSet ||
-                           cruiseReset != lastCruiseReset ||
-                           currentDriveMode != lastDriveMode ||
-                           directionSwitch != lastDirectionSwitch ||
-                           currentOdo != lastOdo;
+  bool screenNeedsUpdate =
+      !hasPreviousFrame || speedValue != lastSpeedValue ||
+      accelPercent != lastAccelPercent || regenPercent != lastRegenPercent ||
+      headlight != lastHeadlight || leftBlink != lastLeftBlink ||
+      rightBlink != lastRightBlink || hazardState != lastHazards ||
+      cruiseMode != lastCruiseMode || cruiseSet != lastCruiseSet ||
+      cruiseReset != lastCruiseReset || currentDriveMode != lastDriveMode ||
+      directionSwitch != lastDirectionSwitch || currentOdo != lastOdo;
 
   if (!screenNeedsUpdate) {
     return;
@@ -223,15 +222,18 @@ void renderMinimalDisplay(float speed) {
   tft.setCursor(12, 10);
   tft.print("SPEED");
 
-  drawStatusPill(128, 6, 82, 28, modeText, currentDriveMode, currentDriveMode ? TFT_RED : TFT_GREEN);
+  drawStatusPill(128, 6, 82, 28, modeText, currentDriveMode,
+                 currentDriveMode ? TFT_RED : TFT_GREEN);
   drawStatusPill(220, 6, 82, 28, directionText, directionSwitch, TFT_CYAN);
-  drawStatusPill(312, 6, 82, 28, "CRZ", cruiseMode || cruiseSet || cruiseReset, TFT_YELLOW);
+  drawStatusPill(312, 6, 82, 28, "CRZ", cruiseMode || cruiseSet || cruiseReset,
+                 TFT_YELLOW);
 
-  char odoBuffer[8];
+  char odoBuffer[16];
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
   tft.setTextSize(3);
   tft.setCursor(10, 90);
-  snprintf(odoBuffer, sizeof(odoBuffer), "%lu", (unsigned long)currentOdo);
+  snprintf(odoBuffer, sizeof(odoBuffer), "%lu.%lu",
+           (unsigned long)(currentOdo / 10), (unsigned long)(currentOdo % 10));
   tft.print(odoBuffer);
 
   tft.setTextSize(2);
@@ -248,7 +250,7 @@ void renderMinimalDisplay(float speed) {
 
   tft.setTextSize(2);
   tft.setTextColor(TFT_DARKGREY, TFT_BLACK);
-  tft.setCursor((tft.width() - tft.textWidth("MPH"))/2, 120);
+  tft.setCursor((tft.width() - tft.textWidth("MPH")) / 2, 120);
   tft.print("MPH");
 
   drawAccelBar(360, 58, 88, 170, accelPercent);
