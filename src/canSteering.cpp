838fbe27ed27d9ccdf48a975995d3a90a391bebf
Will Wentink <wwentink@wisc.edu>
2026-07-19 01:12:15 -0500

    hope this works

--- src/canSteering.cpp
+++ src/canSteering.cpp
@@ -13,6 +13,10 @@ volatile uint8_t can_last_dlc = 0;
 volatile bool battery_fault_active = false;
 volatile float battery_soc = 0.0f;
 
+static volatile bool bps_src_505 = false;
+// static volatile bool bps_src_506 = false;
+// static volatile bool bps_src_507 = false;
+
 CANSteering::CANSteering(int8_t tx, int8_t rx, uint16_t tx_queue, uint16_t rx_queue, uint16_t frequency) : ESP32CANManager(tx, rx, tx_queue, rx_queue, frequency) {};
 void CANSteering::readHandler(CanFrame msg) {
     // 1. Prepare local variables to update under spinlock
@@ -20,13 +24,18 @@ void CANSteering::readHandler(CanFrame msg) {
     uint8_t local_last_dlc = msg.data_length_code;
 
     float local_battery_soc = -1.0f;
-    bool local_battery_fault_active = false;
-    bool update_battery_fault = false;
     bool update_battery_soc = false;
     float local_stuff = 0.0f;
     bool update_stuff = false;
     float local_speedsig = 0.0f;
     bool update_speedsig = false;
+
+    bool local_505 = false;
+    bool update_505 = false;
+    // bool local_506 = false;
+    // bool update_506 = false;
+    // bool local_507 = false;
+    // bool update_507 = false;
 
     switch (msg.identifier){
         case 0x101:{
@@ -40,10 +49,6 @@ void CANSteering::readHandler(CanFrame msg) {
             break;
         }
         case 0x200:{
-            if (msg.data_length_code > 0) {
-                local_battery_fault_active = (msg.data[0] & 0x01U) != 0;
-                update_battery_fault = true;
-            }
             if (msg.data_length_code >= sizeof(float)) {
                 memcpy(&local_stuff, msg.data, sizeof(float));
                 update_stuff = true;
@@ -86,6 +91,28 @@ void CANSteering::readHandler(CanFrame msg) {
             }
             break;
         }
+        case 0x505: {
+            // Powertrain estop
+            if (msg.data_length_code > 0) {
+                local_505 = msg.data[0] != 0;
+                update_505 = true;
+            }
+            break;
+        }
+        // case 0x506: {
+        //     if (msg.data_length_code > 0) {
+        //         local_506 = msg.data[0] != 0;
+        //         update_506 = true;
+        //     }
+        //     break;
+        // }
+        // case 0x507: {
+        //     if (msg.data_length_code > 0) {
+        //         local_507 = msg.data[0] != 0;
+        //         update_507 = true;
+        //     }
+        //     break;
+        // }
         default:
             break;
     }
@@ -98,9 +125,16 @@ void CANSteering::readHandler(CanFrame msg) {
     if (update_battery_soc) {
         battery_soc = local_battery_soc;
     }
-    if (update_battery_fault) {
-        battery_fault_active = local_battery_fault_active;
+    if (update_505) {
+        bps_src_505 = local_505;
     }
+    // if (update_506) {
+    //     bps_src_506 = local_506;
+    // }
+    // if (update_507) {
+    //     bps_src_507 = local_507;
+    // }
+    battery_fault_active = bps_src_505 /* || bps_src_506 || bps_src_507 */;
     if (update_stuff) {
         stuff = local_stuff;
     }
@@ -118,6 +152,7 @@ void CANSteering::sendSteeringData() {
     Digital_Data local_digital_data = const_cast<Digital_Data&>(digital_data);
     uint8_t local_drive_mode = drive_mode;
     bool local_hazards = hazards;
+    bool local_battery_fault_active = battery_fault_active;
     portEXIT_CRITICAL(&stateMux);
 
     send_success = true;
@@ -158,5 +193,9 @@ void CANSteering::sendSteeringData() {
 
     bool hazard_blink = local_hazards && blink_phase;
     tx_ok = this->sendMessage(0x304, (void*)&hazard_blink, sizeof(bool), CAN_SEND_TIMEOUT_MS);
+    send_success &= tx_ok;
+
+    uint8_t bps_light = local_battery_fault_active ? 1 : 0;
+    tx_ok = this->sendMessage(0x100, (void*)&bps_light, sizeof(bps_light), CAN_SEND_TIMEOUT_MS);
     send_success &= tx_ok;
 }
